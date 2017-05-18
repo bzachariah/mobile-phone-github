272b01bb362a07e4c27a9646dad896285284f33b
bzachariah <binitha_zachariah@yahoo.com>
2017-05-18 14:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "mobile-phone-action":
+    #if req.get("result").get("action") != "mobile-phone-action":
+    if req.get("result").get("action") != "yahooWeatherForecast":    
         return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     #baseurl = ""
