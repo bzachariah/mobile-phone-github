ad8accd88eca63bd37e8bf622d8a261ce0bd4982
bzachariah <binitha_zachariah@yahoo.com>
2017-05-18 22:13:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,14 +47,7 @@ def processRequest(req):
     display = parameters.get("display")
   
 
-
-    yql_query ="select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + "London" + "')"
-
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(os, display, data)
+    res = makeWebhookResult(os, display)
     return res
 
 
@@ -62,34 +55,8 @@ def processRequest(req):
     
 
 
-def makeWebhookResult(os, display, data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-    #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+def makeWebhookResult(os, display):
+    
     speech = os + " " + display
     print("Response:")
     print(speech)
