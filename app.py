e1142792c85e4dd49f1d79830ad9e8c0f356bbf8
bzachariah <binitha_zachariah@yahoo.com>
2017-05-18 15:22:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def processRequest(req):
         return {}
     #if req.get("result").get("action") != "yahooWeatherForecast":    
     #    return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    #baseurl = ""
+    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = ""
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
@@ -55,54 +55,28 @@ def processRequest(req):
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    #my_OS = parameters.get("OS")
-    #if my_OS is None:
-    #    return None
-    if city is None:
+    #city = parameters.get("geo-city")
+    my_OS = parameters.get("os")
+    if my_OS is None:
         return None
+    #if city is None:
+    #    return None
 
-    #return my_OS
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return my_OS
+    #return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+    
     print("Response:")
-    print(speech)
-    #print(data)
+    #print(speech)
+    print(data)
 
     return {
-        "speech": speech,
-        #"speech": data
-        #"displayText": data,
-        "displayText": speech,
+        #"speech": speech,
+        "speech": data
+        "displayText": data,
+        #"displayText": speech,
         # "data": data,
         # "contextOut": [],
         "source": "bzachariah/mobile-phone-github"
