2279628da6629a06cf6e9f0d27f9b86f5b3853aa
bzachariah <binitha_zachariah@yahoo.com>
2017-05-18 20:41:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def makeWebhookResult(data):
         "speech": speech,
         "displayText": speech,
         # "data": data,
+        "data": {"telegram": {speech}},
         # "contextOut": [],
         # "source": "apiai-weather-webhook-sample"
         "source": "bzachariah/mobile-phone-github"
